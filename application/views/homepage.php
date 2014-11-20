ebae8ef6e3d20382455b92b2f1e8941d84be3455
Okitora <chris.m.c.lee@hotmial.com>
2014-11-20 13:46:07 -0800

    asn template

---
+++ application/views/homepage.php
@@ -0,0 +1,12 @@
+{places}
+<div class="row">
+    <div class="span11">
+        <h2>{name}</h2>
+    </div>
+    <a class="fancybox-effects-d" href="/data/{pic}" title="{name}"><img class="span5" src="/data/{pic}" alt="" /></a>
+    <div class="span7">
+        <p>{description}</p><br/>
+    </div>
+ 
+</div>
+{/places}
