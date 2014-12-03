c06a645fc96130766ddb895ff82738b97ceefb6f
sharonpvs <sharonpvs@hotmail.com>
2014-12-03 09:46:19 -0800

    took login off home page

--- application/views/newest.php
+++ application/views/newest.php
@@ -5,16 +5,6 @@
         <img src="/data/{pic}" title="{name}"/>
     </div>
     <div class="span6">
-        
-        <!-- form to edit a menu item -->
-        <form action="/authenticate/login/{userid}" method="post">
-            {fusername}
-            {fpassword}
-            {fsubmit}
-        </form>
-
-    </div>
-    <div class="span6">
         <p class="lead">{description}</p>
     </div>
     
