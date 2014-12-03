9bc71e395440aaf6386dbc928f076345428a76ab
Okitora <chris.m.c.lee@hotmial.com>
2014-12-02 18:35:46 -0800

    xml_desc updated

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -3,7 +3,7 @@
     <div class="span11">
         <h2>{name}</h2>
     </div>
-    <a class="fancybox-effects-d" href="/data/{pic}" title="{name}"><img class="span5" src="/data/{pic}" alt="" /></a>
+    <a class="lightbox" href="/data/{pic}" title="{name}"><img class="span5" src="/data/{pic}" alt="" /></a>
     <div class="span7">
         <p>
             {description}
