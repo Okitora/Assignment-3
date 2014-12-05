f07eae27967d3bb618b24626ed93c15a9775779c
sharonpvs <sharonpvs@hotmail.com>
2014-12-05 12:57:07 -0800

    getting ready for new db :)

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -4,13 +4,21 @@
         <h2>{name}</h2>
     </div>
     <a class="lightbox" href="/data/{pic}" title="{id}"><img class="span5" src="/data/{pic}" alt="" /></a>
+    <!--
+    <a class="lightbox" href="/data/{pic2}" title="{pic2}"><img class="span5" src="/data/{pic2}" alt="" /></a>
+    <a class="lightbox" href="/data/{pic3}" title="{pic3}"><img class="span5" src="/data/{pic3}" alt="" /></a>
+    -->
     <div class="span7">
         <p>
             {description}
             <br/>
-        <h5>Main Category: {main_id}</h5>
-        <h5>Target Audience: {target}</h5>
-            <h5>Price Range: {price_range}</h5>
+        <h5>Main Category:</h5> {main_id}<br>
+        <h5>Target Audience:</h5> {target}<br>
+            <h5>Price Range:</h5> {price_range}
+          <!--  <h5>Contact:</h5> {contact}
+            <h5>Price:</h5>{price}
+            <h5>Date:</h5>{date}
+        -->
         </p><br/>
     </div>
  
