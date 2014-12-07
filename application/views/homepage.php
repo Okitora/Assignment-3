1a9c33a0d5c389ba1fe4f3c1b8df9f512d207942
sharonpvs <sharonpvs@hotmail.com>
2014-12-07 14:00:08 -0800

    changed homepage

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -4,9 +4,14 @@
         <h2>{name}</h2>
     </div>
     
-    <tr>
+    <div class="span5">
     <a href="/data/{pic1}" title="{id}_{pic1}"><img class="span5" src="/data/{pic1}" alt="" /></a>
-    </tr>
+    <br>
+        <a href="/data/{pic2}" title="{id}_{pic2}"><img class="span5" src="/data/{pic2}" alt="" /></a>
+        <br>
+    <a href="/data/{pic3}" title="{id}_{pic3}"><img class="span5" src="/data/{pic3}" alt="" /></a>
+    <br/>
+    </div>
     <tr>
     <div class="span7">
         <p>
@@ -14,12 +19,12 @@
         </p>
         <table>
             <tr>
-                <td><h5>Main Category:</h5></td>
-                <td><h5>Target Audience:</h5></td>
-                <td><h5>Price Range:</h5></td>
-                <td><h5>Contact:</h5></td> 
-                <td><h5>Price:</h5></td>
-                <td><h5>Date:</h5> </td>
+                <td><h5>Main Category </h5></td>
+                <td><h5>Target Audience </h5></td>
+                <td><h5>Price Range </h5></td>
+                <td><h5>Contact </h5></td> 
+                <td><h5>Price </h5></td>
+                <td><h5>Date </h5> </td>
             </tr>
             <tr>
                 <td>{main_id}</td>
@@ -30,22 +35,18 @@
                 <td>{date}</td>    
             </tr>
         </table>
-        <table>
+        
             {specifics}
-            <tr>
-                <td><h5>{firstName}:</h5></td>
-                <td>{firstVal}</td>
-            </tr>
-            <tr>
-                <td><h5>{secondName}:</h5></td>
-                <td>{secondVal}</td>
-            </tr>
+            
+        <h5>{firstName}: {firstVal}</h5>
+            
+            
+            
+        <h5>{secondName}: {secondVal}</h5>
+            
+            
             {/specifics}
-        </table>
-        <br>
-        <a href="/data/{pic2}" title="{id}_{pic2}"><img class="span5" src="/data/{pic2}" alt="" /></a>
-        <a href="/data/{pic3}" title="{id}_{pic3}"><img class="span5" src="/data/{pic3}" alt="" /></a>
-        <br/>
+        
     </div>
     </tr>
 </div>
